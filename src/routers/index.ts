c1e3662d94656e299b7430d3dcecfef6b72431dd
ZLoganZ <ln26805@gmail.com>
2023-12-22 10:35:07 +0700

    Comment out Authentication middleware in index.ts

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -9,7 +9,7 @@ import { Authentication } from '../middlewares';
 const router = Router();
 
 router.use('/auth', authRouter);
-router.use(Authentication);
+// router.use(Authentication);
 router.use('/users', userRouter);
 router.use('/posts', postRouter);
 router.use('/comments', commentRouter);
